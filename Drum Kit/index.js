dbc20dbacd81fd9e474cf28d67f6ada570eedf45
vanessaezeoke <vanessa.ezeoke@gmail.com>
2022-01-15 11:47:12 +0100

    Used Time out functions for animation, changed solution to react to clicks as well as key presses.

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -2,43 +2,57 @@
 for (var i=0; i<document.querySelectorAll(".drum").length; i++)
   {
   document.querySelectorAll(".drum")[i].addEventListener("click",
-  function (){
-    switch (this.innerHTML) {
-      case "w":
-          new Audio("sounds/crash.mp3").play()
-      break;
-      case "a":
-          new Audio("sounds/tom-1.mp3").play()
-      break;
-      case "s":
-          new Audio("sounds/tom-2.mp3").play()
-      break;
-      case "d":
-          new Audio("sounds/tom-3.mp3").play()
-      break;
-      case "j":
-          new Audio("sounds/tom-4.mp3").play()
-      break;
-      case "k":
-          new Audio("sounds/kick-bass.mp3").play()
-      break;
-      case "l":
-          new Audio("sounds/snare.mp3").play()
-      break;
-
-      default: console.log(this.innerHTML)
-
+    function (){
+    makeSound(this.innerHTML);
+    buttonAnime(this.innerHTML);
     }
 
-  })
+  )
 }
 
-/*document.querySelector("button").addEventListener("click", handleClick);
+document.addEventListener("keydown", function(event){
+  makeSound(event.key);
+  buttonAnime(event.key);
+})
 
-function handleClick(){
-  alert("I got clicked");
-}*/
+//keydown and keypress both work in the above
 
-/*alert("I got clicked");
-new Audio("sounds/tom-1.mp3").play();
-console.log(this.innerHTML);*/
+
+function makeSound(key){
+  switch (key) {
+    case "w":
+        new Audio("sounds/crash.mp3").play()
+    break;
+    case "a":
+        new Audio("sounds/tom-1.mp3").play()
+    break;
+    case "s":
+        new Audio("sounds/tom-2.mp3").play()
+    break;
+    case "d":
+        new Audio("sounds/tom-3.mp3").play()
+    break;
+    case "j":
+        new Audio("sounds/tom-4.mp3").play()
+    break;
+    case "k":
+        new Audio("sounds/kick-bass.mp3").play()
+    break;
+    case "l":
+        new Audio("sounds/snare.mp3").play()
+    break;
+
+    default: console.log(key)
+}
+}
+
+function buttonAnime(currentKey){
+  var activeKey= document.querySelector("." + currentKey);
+  activeKey.classList.add("pressed");
+
+  setTimeout(function() {
+    activeKey.classList.remove("pressed");
+  }, 100);
+
+
+}
